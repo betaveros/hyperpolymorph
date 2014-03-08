46fd96fb1b0ddd906b8de6b35c76acc8e1757fcc
Brian Chen <brian.chxn@gmail.com>
2014-03-08 11:01:14 +0800

    Minimal working example, with several simple Python entries

--- morph.hs
+++ morph.hs
@@ -9,15 +9,16 @@ import Text.Parsec.Prim
 
 -- utility HTML-making functions
 
-makeTag tagname cls cont =
-	"<" ++ tagname ++ " class='" ++ cls ++ "'>" ++ cont ++ "</" ++ tagname ++ ">"
-
-makeSpanTag = makeTag "span"
-makeStrongTag = makeTag "strong"
+makeSpanTag cls cont =
+	"<span class='" ++ cls ++ "'>" ++ cont ++ "</span>"
+makeStrongTag cont =
+	"<strong>" ++ cont ++ "</strong>"
 
 -- parsing
 
-isID c = isAlphaNum c || c == '_'
+isId c = isAlphaNum c || c == '_'
+idChar :: CharParser Char
+idChar = satisfy isId
 
 type CharParser = Parsec [Char] ()
 
@@ -28,18 +29,20 @@ bqAtom :: CharParser String
 bqAtom = bqSpecial <|> plainAtom
 
 plainAtom :: CharParser String
-plainAtom = htmlEscaped <|> many1 (satisfy isID) <|> fmap return anyChar
+plainAtom = htmlEscaped <|> many1 idChar <|> fmap return (noneOf "`")
 
 htmlEscaped :: CharParser String
 htmlEscaped = esc '&' "&amp;" <|> esc '<' "&lt;" <|> esc '>' "&gt;" <|> esc '\n' "<br />"
 	where esc c s = char c >> return s
 
 bqSpecial :: CharParser String
-bqSpecial = char '`' >>
+bqSpecial = try $ char '`' >>
 	((char '`' >> return "`")
 	<|> (char '[' >> return "[")
 	<|> (char ']' >> return "]")
+	<|> (char '*' >> fmap makeStrongTag bqGroup)
 	<|> (char '!' >> fmap (makeSpanTag "key") bqGroup)
+	<|> (char '.' >> fmap (makeSpanTag "builtin") bqGroup)
 	<|> (char '=' >> fmap (makeSpanTag "val") bqGroup)
 	<|> (fmap (makeSpanTag "aside") $ plainMatching '(' ')')
 	)
@@ -61,12 +64,94 @@ bqGroup = (bqMatching '[' ']') <|> bqAtom
 
 parseBqMarkup input = parse bqMarkup "(unknown)" input
 
+bqEntry :: CharParser (String, String)
+bqEntry = do
+	string "`e"
+	many1 (oneOf " \t")
+	name <- many1 idChar
+	many1 space
+	cont <- bqMarkup
+	return (name, cont)
+
+type EntryMap = M.Map String String
+
+bqEntryMap :: CharParser EntryMap
+bqEntryMap = do
+	entries <- many bqEntry
+	return $ M.fromList entries
+
+-- parse the entry associations used to create final output
+
+bqEntryAssoc :: CharParser (String, String)
+bqEntryAssoc = do
+	key <- many1 idChar
+	char ':'
+	many1 (oneOf " \t")
+	desc <- many1 (noneOf "\n")
+	newline
+	return (key, desc)
+
+bqEntryHeading :: CharParser String
+bqEntryHeading = do
+	char '#'
+	spaces
+	heading <- many1 (noneOf "\n")
+	newline
+	return heading
+
+type Section = (String, [(String, String)])
+
+bqEntrySection :: CharParser Section
+bqEntrySection = do
+	heading <- bqEntryHeading
+	assocs <- many bqEntryAssoc
+	return (heading, assocs)
+
+bqSectionList :: CharParser [Section]
+bqSectionList = many bqEntrySection
+
+-- make everything together!
+
+buildTd :: String -> EntryMap -> String
+buildTd key emap = case M.lookup key emap of
+	Nothing -> "<td class='empty'></td>"
+	Just s -> "<td>" ++ s ++ "</td>"
+
+buildRow :: (String, String) -> [EntryMap] -> String
+buildRow (key, desc) emaps =
+	"<tr><td>" ++ desc ++ "</td>" ++ concatMap (buildTd key) emaps ++ "</tr>"
+
+makeThRow colspan cont =
+	"<tr><th colspan='" ++ show colspan ++ "'>" ++ cont ++ "</th></tr>"
+
+buildSection :: Section -> [EntryMap] -> String
+buildSection (heading, assocs) emaps = unlines (
+	makeThRow (length emaps + 1) heading : [
+		buildRow assoc emaps | assoc <- assocs
+	])
+
+buildTable :: [Section] -> [EntryMap] -> String
+buildTable secs emaps = unlines (["<table>"]
+	++ [buildSection sec emaps | sec <- secs]
+	++ ["</table>"])
+
 startHTML = unlines [
 	"<html>",
 	"<head>",
 	"<title>Hyperpolymorph</title>",
-	"<link rel='stylesheet' href='hyperpolymorph.css' />",
+	"<link rel='stylesheet' href='morph.css' />",
 	"</head>",
 	"<body>"
 	]
 endHTML = unlines ["</body>", "</html>"]
+
+parseIO parser txt = either (ioError . userError . show) return $ parse parser "???" txt
+
+main = do
+	cont <- readFile "entries.txt"
+	secs <- parseIO bqSectionList cont
+	pycont <- readFile "python.txt"
+	pymap <- parseIO bqEntryMap pycont
+	putStrLn startHTML
+	putStrLn $ buildTable secs [pymap]
+	putStrLn endHTML
