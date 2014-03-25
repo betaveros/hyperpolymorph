5d5d29b6c5ccd5786e590f52a927d6c353fc7002
Brian Chen <brian.chxn@gmail.com>
2014-03-25 17:17:44 +0800

    Tentative addition of ML Dialects and Friends

--- harvest.py
+++ harvest.py
@@ -14,34 +14,42 @@ def textish(node):
 
 overrides = {
 	"statement-separator": "statement-terminator",
+	"blocks": "block-delimiters",
+	"modifiable-variable": "local-variable",
 	"write-once-variable": "constant",
 	"arithmetic-operators-addition-subtraction-multiplication-float-division-quotient-remainder": "arithmetic-operators",
 	"compound-assignment-arithmetic-bit": "compound-assignment-arithmetic-string-logical-bit",
+	"integer-division-and-remainder": "divmod",
 	"transcendental-constants-and-e": "transcendental-constants",
 	"float-truncation-towards-zero-to-nearest-integer-towards-towards": "float-truncation",
 	"float-truncation-round-towards-zero-round-to-nearest-integer-round-down-round-up": "float-truncation",
+	"random-number-uniform-int-uniform-float-normal-float": "random-number-uniform-integer-uniform-float-normal-float",
 	"random-seed-set-get-restore": "random-seed",
 	"random-seed-how-to-set": "random-seed",
 	"bit-operators-left-shift-right-shift-and-inclusive-or-exclusive-or-complement": "bit-operators",
+	"radix": "radix-convert-integer-to-and-from-string-with-radix",
 	"character-escapes": "literal-escapes",
 	"translate-case-to-upper-to-lower": "translate-case",
 	"case-manipulation": "translate-case",
 	"format-string": "format",
 	"string-concatenation": "concatenate-strings",
 	"trim-both-sides-left-right": "trim",
+	"pad-on-left-on-right": "pad",
 	"pad-on-left-on-right-centered": "pad",
 	"pad-on-right-on-left-centered": "pad",
 	"pad-on-right-on-left": "pad",
 	"length": "string-length",
 	"index-of-substring-first-last": "index-of-substring",
-
 	"extract-substring-by-start-and-length-by-start-and-end-by-successive-starts": "extract-substring",
 	"date-and-time-type": "date-time-type",
+	"date-and-time-types": "date-time-type",
 	"current-date-and-time": "current-date-time",
 	"result-of-date-subtraction": "date-subtraction",
 	"add-time-duration": "add-duration",
 	"date-parts": "date-parts",
 	"time-parts": "time-parts",
+	"empty-list-test": "empty-test",
+	"nth-element": "list-lookup",
 	"element-index": "index-of-element",
 	"shuffle": "shuffle-and-sample",
 	"index-of-element-first-and-last-occurrence": "index-of-element",
@@ -55,7 +63,9 @@ overrides = {
 	"relative-complement": "relative-complement-symmetric-difference",
 	"function-declaration": "declare-function",
 	"function-invocation": "invoke-function",
+	"named-parameter": "named-parameters",
 	"missing-argument-value": "missing-argument-behavior",
+	"if-else-if-else": "control-structure-keywords",
 	"break-and-continue": "break-continue-redo",
 	"copy-address-copy-shallow-copy-deep-copy": "copy",
 	"sort-non-destructive-in-place-custom-comparision": "sort",
@@ -63,6 +73,12 @@ overrides = {
 	"finally-ensure": "finally",
 	"finally-clause": "finally",
 	"namespace-declaration": "declare-namespace",
+	"write-formatted-string-to-stdout": "printf",
+	"file-test-regular-file-test": "file-exists-test-regular-file-test",
+	"temporary-directory": "system-temporary-file-directory",
+	"class-definition": "define-class",
+	"object-creation": "create-object",
+	"method-invocation": "invoke-method",
 }
 def normalize(e):
 	h = re.sub(r'\W+', '-', e.lower()).strip('-')
@@ -112,11 +128,11 @@ def parse(filename, colfilenames):
 	for f in colfiles: f.close()
 
 # to_parse_entries = [('scripting.html', 'entries.txt', False),('cpp.html', 'entries.txt'),('c.html', 'entries.txt'),('pascal.html', 'entries.txt'),('lisp.html', 'entries.txt'),('ml.html', 'entries.txt')]
-# to_parse_entries = [('more.html', 'entries.more.txt', False)]
+# to_parse_entries = [('ml.html', 'entries.ml.txt', False)]
 to_parse_entries = []
 # to_parse = [('scripting.html', ['perl.txt', 'php.txt', 'python.txt', 'ruby.txt']),('more.html', ['tcl.txt', 'lua.txt', 'javascript.txt', 'groovy.txt']),('cpp.html', ['cpp.txt', 'objective-c.txt', 'java.txt', 'c-sharp.txt']),('c.html', ['c.txt', 'go.txt']),('pascal.html', ['pascal.txt', 'ada.txt', 'plpgsql.txt']),('lisp.html', ['common-lisp.txt', 'racket.txt', 'clojure.txt', 'emacs-lisp.txt']),('ml.html', ['ocaml.txt', 'f-sharp.txt', 'scala.txt', 'haskell.txt']),('logic.html', ['prolog.txt', 'erlang.txt']),('stack.html', ['forth.txt', 'postscript.txt', 'factor.txt']),('shell.html', ['posix-shell.txt', 'applescript.txt', 'powershell.txt']),('data.html', ['sql.txt', 'awk.txt', 'pig.txt']),('numerical-analysis.html', ['matlab.txt', 'r.txt', 'numpy.txt']),('fortran.html', ['fortran.txt']),('computer-algebra.html', ['mathematica.txt', 'sympy.txt', 'maxima.txt', 'pari-gp.txt']),]
-to_parse = []
-to_parse = [('more.html', ['tcl.txt', 'lua.txt', 'javascript.txt', 'groovy.txt'])]
+to_parse = [('ml.html', ['ocaml.txt', 'f-sharp.txt', 'scala.txt', 'haskell.txt'])]
+# to_parse = [('more.html', ['tcl.txt', 'lua.txt', 'javascript.txt', 'groovy.txt'])]
 # to_parse = [('lisp.html', ['common-lisp.txt', 'racket.txt', 'clojure.txt', 'emacs-lisp.txt'])]
 
 for ix, t in enumerate(to_parse_entries):
