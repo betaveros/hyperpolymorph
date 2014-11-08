d255fb03a671f5487f4bf1e29c4ee54bfd0c4234
Brian Chen <brian.chxn@gmail.com>
2014-11-08 13:16:47 +0800

    Tidy up, add Ruby

--- morph.hs
+++ morph.hs
@@ -195,7 +195,7 @@ readEntryMap name = readFile (name ++ ".txt") >>= parseIO bqEntryMap name
 readNamedEntryMap name = (,) name <$> readEntryMap name
 
 -- langNames = ["perl","php","python","ruby","tcl","lua","javascript","groovy","cpp","objective-c","java","c-sharp","c","go","pascal","ada","plpgsql","common-lisp","racket","clojure","c-sharp","ocaml","f-sharp","scala","haskell","prolog","erlang","forth","postscript","factor","posix-shell","applescript","powershell","sql","awk","pig","matlab","r","numpy","fortran","mathematica","sympy","maxima","pari-gp"]
-langNames = ["cpp","d","java","ocaml","haskell","coffeescript","javascript","perl","php","python","scala"]
+langNames = ["ruby","python","javascript","cpp","d","java","ocaml","haskell","coffeescript","perl","php","scala"]
 
 
 main = do
